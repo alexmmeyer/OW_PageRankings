168011512181cafc10c1d1267648e465cb87fabc
Alex <alexmmeyer@gmail.com>
2022-12-10 16:41:02 -0500

    added back missing csv files

--- apps/rankings.py
+++ apps/rankings.py
@@ -136,7 +136,7 @@ def update_title(gender_choice, rank_date, comp_date):
         title_date = ''
         title_change = ''
     else:
-        title_main = f"{gender_choice}'s 10km Marathon Swimming World Rankingssssss"
+        title_main = f"{gender_choice}'s 10km Marathon Swimming World Rankings"
         title_main = title_main.upper()
         title_date = dt.strftime(dt.strptime(rank_date, "%Y-%m-%d"), "%d %B, %Y")
         formatted_comp_date = dt.strftime(dt.strptime(comp_date, "%Y-%m-%d"), "%d %B, %Y")
