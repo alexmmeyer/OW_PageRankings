27d72e45b60f4bc4100f8fe1d6c858f79229365e
Alex <alexmmeyer@gmail.com>
2023-09-24 22:37:33 -0400

    added fukuoka W 10km splits

--- variables.py
+++ variables.py
@@ -1,6 +1,6 @@
 import pandas
 
-GENDER = "men"
+GENDER = "women"
 
 event_weights = pandas.read_csv("event_weights.csv")
 athlete_countries = pandas.read_csv('app_data/' + GENDER + "/athlete_countries.csv")
