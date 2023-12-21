24c80eedc8ea95dd70b899d3a0ddff8efaf1dc5e
Alex <alexmmeyer@gmail.com>
2023-12-21 10:16:48 -0600

    v1 results app

---
+++ apps/results.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import os
+from datetime import datetime as dt
+from datetime import timedelta, date
+import plotly.graph_objs as go
+import plotly.express as px
+from dash import Dash, html, dcc, dash_table
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+
+from app import app
+
+
+def custom_label(race_result_file, *args):
+    race_data = pd.read_csv(race_result_file)
+    race_label = ""
+    for arg in args:
+        race_label = race_label + str(race_data[arg][0]) + " "
+    return race_label.strip()
+
+
+layout = html.Div([
+    dcc.RadioItems(id='results-gender-picker',
+                   options=[{'label': 'Men', 'value': 'men'}, {'label': 'Women', 'value': 'women'}],
+                   labelStyle={'margin-left': '20px'},
+                   # value='men',
+                   persistence=True, persistence_type='session'),
+    dcc.Dropdown(id='results-race-dropdown'),
+    html.Div(id='results-output-table', children='this is where the results table will go')
+])
+
+@app.callback(Output('results-race-dropdown', 'options'),
+              [Input('results-gender-picker', 'value')])
+def list_names(gender_choice):
+    results_path = 'app_data/' + gender_choice + "/results"
+    race_choices = os.listdir(results_path)
+    return [{'label': i, 'value': i} for i in race_choices]
+
+@app.callback(Output('results-output-table', 'children'),
+              [Input('results-gender-picker', 'value'),
+               Input('results-race-dropdown', 'value')])
+def insert_results(gender_choice, race_choice):
+    results_data = pd.read_csv('app_data/' + gender_choice + "/results/" + race_choice)
+    data = results_data.to_dict('rows')
+    columns = [{"name": i, "id": i, } for i in results_data.columns]
+    # columns = [
+    #     {"name": 'Date', "id": 'dt_date'},
+    #     {"name": 'Event', "id": 'event'},
+    #     {"name": 'Location', "id": 'location'},
+    #     {"name": 'Distance (km)', "id": 'distance'},
+    #     {"name": 'Place', "id": 'place'},
+    #     {"name": 'Field Size', "id": 'field_size'},
+    # ]
+    table = [dash_table.DataTable(data=data, columns=columns, sort_action="native", sort_mode="multi",
+                                  style_as_list_view=True)]
+    return table
+
+
--- index.py
+++ index.py
@@ -7,7 +7,7 @@ from app import app
 from app import server
 
 # Connect to your app pages
-from apps import profile, head2head, progressions, rankings, race_trends
+from apps import profile, head2head, progressions, rankings, race_trends, results
 
 link_style = {'display': 'inline', 'padding-left': '40px', 'padding-right': '40px'}
 
@@ -18,7 +18,8 @@ app.layout = html.Div([
         dcc.Link('ATHLETE PROFILES', href='/apps/profile'),
         dcc.Link('RANKING PROGRESSIONS', href='/apps/progressions'),
         dcc.Link('RACE TRENDS', href='/apps/race_trends'),
-        dcc.Link('HEAD-TO-HEAD', href='/apps/head2head')
+        dcc.Link('HEAD-TO-HEAD', href='/apps/head2head'),
+        dcc.Link('RESULTS', href='/apps/results'),
     ], className="row", style=link_style),
     html.Div(id='page-content', children=[], style={'padding-left': '40px', 'padding-right': '40px'})
 ])
@@ -30,7 +31,8 @@ default = html.Div(
         html.Li('ATHLETE PROFILES: Look up summary stats, world ranking, and results for a single athlete.'),
         html.Li('RANKING PROGRESSIONS: Compare ranking progressions of multiple athletes in one figure.'),
         html.Li('RACE TRENDS: See where in the pack an athlete was positioned at various points throughout a race.'),
-        html.Li('HEAD-TO-HEAD: Match up two athletes to see a win/loss record and finish time differences.')
+        html.Li('HEAD-TO-HEAD: Match up two athletes to see a win/loss record and finish time differences.'),
+        html.Li('RESULTS: See race results details.'),
     ])]
 )
 
@@ -40,6 +42,7 @@ layouts = {
     '/apps/profile': profile.layout,
     '/apps/rankings': rankings.layout,
     '/apps/race_trends': race_trends.layout,
+    '/apps/results': results.layout,
     '/': default
 }
 
