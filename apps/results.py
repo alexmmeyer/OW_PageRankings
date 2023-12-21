e2f449867fbbebf684849d6e80d8bb5296ebc932
Alex <alexmmeyer@gmail.com>
2023-12-21 11:43:29 -0500

    v1 results app

--- apps/results.py
+++ apps/results.py
@@ -15,7 +15,11 @@ def custom_label(race_result_file, *args):
     race_data = pd.read_csv(race_result_file)
     race_label = ""
     for arg in args:
-        race_label = race_label + str(race_data[arg][0]) + " "
+        race_label = race_label + str(race_data[arg][0])
+        if arg == 'distance':
+            race_label = race_label + "km "
+        else:
+            race_label = race_label + " "
     return race_label.strip()
 
 
@@ -32,15 +36,20 @@ layout = html.Div([
 @app.callback(Output('results-race-dropdown', 'options'),
               [Input('results-gender-picker', 'value')])
 def list_names(gender_choice):
-    results_path = 'app_data/' + gender_choice + "/results"
+    results_path = 'app_data/' + gender_choice + '/results'
     race_choices = os.listdir(results_path)
-    return [{'label': i, 'value': i} for i in race_choices]
+    # for i in inrace_choices:
+        # print(custom_label('app_data/' + gender_choice + '/results/' + i, 'event', 'location', 'distance', 'date'))
+
+    return [{'label': custom_label('app_data/' + gender_choice + '/results/' + i, 'event', 'location', 'distance', 'date'), 'value': i} for i in race_choices]
 
 @app.callback(Output('results-output-table', 'children'),
               [Input('results-gender-picker', 'value'),
                Input('results-race-dropdown', 'value')])
 def insert_results(gender_choice, race_choice):
     results_data = pd.read_csv('app_data/' + gender_choice + "/results/" + race_choice)
+    results_data = results_data[['place', 'athlete_name', 'country', 'time']]
+    results_data['time'] = [str(timedelta(seconds=time_in_secs)) for time_in_secs in results_data['time']]
     data = results_data.to_dict('rows')
     columns = [{"name": i, "id": i, } for i in results_data.columns]
     # columns = [
